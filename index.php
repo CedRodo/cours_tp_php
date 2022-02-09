dad77dc65263ae29e6710b9114aa579d6c7477de
Cédric Rodony <cedric.rodony@gmail.com>
2022-02-09 05:12:57 +0100

    nav hover et acces htmlcss

--- index.php
+++ index.php
@@ -4,10 +4,11 @@
 <img src="./assets/logophp.png">
 <p>Nous allons pouvoir utiliser ce site pour répertorier et visualiser les différents cours et exercices effectués en PHP. Ceux-ci sont accessibles via le menu de navigation.
 
-Pour en savoir plus, vous pouvez directement consulter la <a href="https://www.php.net/docs.php">documentation</a> sur le site officiel de <a href="https://www.php.net/">PHP</a>.
+Pour en savoir plus, vous pouvez directement consulter la <a href="https://www.php.net/docs.php" target="_blank">documentation</a> sur le site officiel de <a href="https://www.php.net/" target="_blank">PHP</a>.
 
-</p> 
-<?php include('footer.php'); ?>
+Vous pouvez également accéder à d'autres cours :
+
+<a href ="../courshtmlcss/index.php" target="_blank">HTML/CSS</a>
+</p>
 </div>
-</body>
-</html>
+<?php include('footer.php'); ?>
